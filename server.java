cb289e6a48b72e47391cedcbf3dbf58adfd14257
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2024-01-22 11:38:13 +0530

    Update server.java

--- server.java
+++ server.java
@@ -1,19 +1,21 @@
+//server.java
+
 import java.net.*;
 import java.io.*;
-public class server{
+public class one{
 	public static void main(String[]args) throws Exception{
 		ServerSocket sersock=new ServerSocket(4000);
 		System.out.println("Server ready for communication");
 		Socket sock = sersock.accept();
 		System.out.println("Connection successful and waiting to serve");
 		InputStream istream=sock.getInputStream();
-		BufferedReader fileRead = new BufferedReader(newInputStreamReader(istream));
+		BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
 		String fname=fileRead.readLine();
 		BufferedReader contentRead = new BufferedReader(new FileReader(fname));
 		OutputStream ostream=sock.getOutputStream();
 		PrintWriter pwrite=new PrintWriter(ostream,true);
 		String str;
-		while((str=contentRead.readLine()!=null){
+		while((str=contentRead.readLine())!=null){
 			pwrite.println(str);
 		}
 	}
